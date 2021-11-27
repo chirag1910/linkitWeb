73e8d5132a1806dd02d09d6427e5c36b8d5654de
chirag <chiragindia2@gmail.com>
2021-11-28 04:01:05 +0530

    Added empty public group indicator if no url present

--- Components/PublicGroup.js
+++ Components/PublicGroup.js
@@ -7,13 +7,19 @@ const PublicGroup = ({ title, urls }) => {
                 <h1>{title}</h1>
             </div>
             <div className={styles.groupBody}>
-                {urls.map((url) => (
-                    <a href={url.fullUrl} key={url.urlID} target="_blank">
-                        <div className={styles.urlCard}>
-                            <h3>{url.title}</h3>
-                        </div>
-                    </a>
-                ))}
+                {!urls.length ? (
+                    <div className={styles.noUrl}>
+                        <h1>Group empty !!</h1>
+                    </div>
+                ) : (
+                    urls.map((url) => (
+                        <a href={url.fullUrl} key={url.urlID} target="_blank">
+                            <div className={styles.urlCard}>
+                                <h3>{url.title}</h3>
+                            </div>
+                        </a>
+                    ))
+                )}
             </div>
         </div>
     );
