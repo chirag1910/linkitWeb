d714b0111367cbc3d16637dd126851a7bde4a333
chirag <chiragindia2@gmail.com>
2021-11-28 13:04:28 +0530

    Added progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import "../styles/nprogress.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import Initialize from "../initialize/Initialize";
+import Router from "next/router";
+import nProgress from "nprogress";
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
 
 function MyApp({ Component, pageProps }) {
     return (
